8000a89b3d5900ce4538957569e0817a8f0dac3a
Lars Lorentz Ludvigsen <larsll@outlook.com>
2020-03-14 13:10:00 +0000

    Revert "Alter sagemaker python SDK to accept more env variables"
    This reverts commit 656acdfce6ece9306d82208c7f20b4db24e64e52.

--- src/sagemaker/local/local_session.py
+++ src/sagemaker/local/local_session.py
@@ -31,9 +31,6 @@ from sagemaker.local.entities import (
 from sagemaker.session import Session
 from sagemaker.utils import get_config_value, _module_import_error
 
-import json
-import os
-
 logger = logging.getLogger(__name__)
 
 
@@ -409,21 +406,6 @@ class LocalSession(Session):
 
         self.sagemaker_client = LocalSagemakerClient(self)
         self.sagemaker_runtime_client = LocalSagemakerRuntimeClient(self.config)
-        self.environment_variables = {}
-        self.extra_docker_content = {}
-
-        # Load extra environment_variables from a file
-        _env_vars_file = os.environ.get("LOCAL_ENV_VAR_JSON_PATH", None)
-        if _env_vars_file != None:
-            with open(_env_vars_file) as f:
-                self.environment_variables = json.load(f)
-
-        _extra_docker_content = os.environ.get("LOCAL_EXTRA_DOCKER_COMPOSE_PATH", None)
-        if _extra_docker_content != None:
-            with open(_extra_docker_content) as f:
-                self.extra_docker_content = json.load(f)
-
-
         self.local_mode = True
 
         sagemaker_config_file = os.path.join(os.path.expanduser("~"), ".sagemaker", "config.yaml")
